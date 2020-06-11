4848ab85c0d36faeb6289c398da26e191f1fd092
Mani Singh <singh@rciadmins-MacBook-Pro.local>
2020-06-11 14:18:17 -0400

    Change Values Type in helm chart

--- restartr/api.py
+++ restartr/api.py
@@ -2,38 +2,38 @@ import argparse
 import json
 import logging
 import os
+import pymongo
+import requests
 import sys
 import time
 import traceback
-
-from bson import ObjectId
+import yaml
+import uuid
 from flasgger import Swagger
-from flask import Flask, request
+from flask import Flask, jsonify, g, Response, request
+from flask_restful import Api, Resource
 from flask_cors import CORS
 from flask_pymongo import PyMongo
-from flask_restful import Api, Resource
-
+from bson import ObjectId
 
 class JSONEncoder(json.JSONEncoder):
     """ JSON Encodes Mongo ObjectId. """
-
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
-
 """
 Defines the restarting research genomics data API.
 """
-logger = logging.getLogger(__name__)
+logger = logging.getLogger (__name__)
 
 app = Flask(__name__)
 
 """ Enable CORS. """
 api = Api(app)
 CORS(app)
-debug = False
+debug=False
 
 """ Describe the API. """
 app.config['SWAGGER'] = {
@@ -47,75 +47,66 @@ swagger = Swagger(app)
 mongo_uri = os.environ['MONGO_URL']
 auth_src = "?authSource=admin&ssl=false&ssl_cert_reqs=CERT_NONE"
 observation = PyMongo(app, uri=f"{mongo_uri}/observation{auth_src}", ssl=False)
-
-
-# contact = PyMongo(app, uri=f"{mongo_uri}/contact{auth_src}")
-# survey = PyMongo(app, uri=f"{mongo_uri}/survey{auth_src}")
+#contact = PyMongo(app, uri=f"{mongo_uri}/contact{auth_src}")
+#survey = PyMongo(app, uri=f"{mongo_uri}/survey{auth_src}")
 
 class APIKey:
     """ Use environment provided API Key to secure services. """
-
     @staticmethod
-    def initialize():
-        api_key = observation.db.observation.find_one({"api_key": "true"})
+    def initialize ():
+        api_key = observation.db.observation.find_one ({ "api_key" : "true" })
         if api_key:
             api_key = api_key['value']
         else:
-            api_key = os.environ['API_KEY']
-            observation.db.observation.insert_one({
-                "api_key": "true",
-                "value": api_key
+            api_key = os.environ ['API_KEY']
+            observation.db.observation.insert_one ({
+                "api_key" : "true",
+                "value"   : api_key
             })
         return api_key
-
     @staticmethod
-    def get_key():
-        return APIKey.initialize()
-
+    def get_key ():
+        return APIKey.initialize ()
 
 for attempt in range(0, 7):
-    logger.info(f"-- attempting to connect to db.")
+    logger.info (f"-- attempting to connect to db.")
     try:
-        api_key = APIKey.initialize()
-        logger.info(f"-- initialized db.")
+        api_key = APIKey.initialize ()
+        logger.info (f"-- initialized db.")
         break
     except Exception as e:
-        traceback.print_exc()
-        logger.info(f"-- unable to connect to db. pausing.")
-        time.sleep(3)
-
+        traceback.print_exc ()
+        logger.info (f"-- unable to connect to db. pausing.")
+        time.sleep (3)
 
 class RestartRResource(Resource):
-    """ Record an observation """
 
+    """ Record an observation """
     def __init__(self, api_key):
         self.api_key = api_key
 
     """ Base class handler for API requests. """
-
-    def create_response(self, result=None, status='success', message='', exception=None):
+    def create_response (self, result=None, status='success', message='', exception=None):
         """ Create a response. Handle formatting and modifiation of status for exceptions. """
         if exception:
-            traceback.print_exc()
-            status = 'error'
+            traceback.print_exc ()
+            status='error'
             exc_type, exc_value, exc_traceback = sys.exc_info()
             result = {
-                'error': repr(traceback.format_exception(exc_type, exc_value, exc_traceback))
+                'error' : repr(traceback.format_exception(exc_type, exc_value, exc_traceback))
             }
         return {
-            'status': status,
-            'result': result,
-            'message': message
+            'status'  : status,
+            'result'  : result,
+            'message' : message
         }
-
 
 class ObservationResource(RestartRResource):
     """ System initiation. """
-
     def post(self):
         """
         Record an observation.
-
+        
         ---
         tag: observation
         description: Add an observation.
@@ -126,6 +117,7 @@ class ObservationResource(RestartRResource):
                 application/json:
                     schema:
                         type: object
+
                         properties:
                           foo:
                             type: string
@@ -153,6 +145,7 @@ class ObservationResource(RestartRResource):
                     text/plain:
                         schema:
                             type: string
+
         parameters:
             - in: body
               name: observation
@@ -167,31 +160,29 @@ class ObservationResource(RestartRResource):
               required: true
               description: The observation to record.
         """
-        logger.debug(f"observation:{json.dumps(request.json, indent=2)}")
+        logger.debug (f"observation:{json.dumps(request.json, indent=2)}")
         response = {}
         try:
             obj = dict(request.json)
             authenticated = request.headers['X-API-Key'] == self.api_key
             assert authenticated, f"API Key presented does not match key configured for this system."
             response = {
-                "id": JSONEncoder().encode(
-                    observation.db.observation.insert_one(obj).inserted_id
+                "id" : JSONEncoder().encode(
+                    observation.db.observation.insert_one (obj).inserted_id
                 )
             }
         except Exception as e:
-            response = self.create_response(
+            response = self.create_response (
                 exception=e,
                 message=f"Insert failed: {json.dumps(request.json, indent=2)}.")
         return response
 
-
 class ObservationQueryResource(RestartRResource):
     """ System initiation. """
-
     def post(self):
         """
         Query observations.
-
+        
         ---
         tag: observation
         description: Query observations.
@@ -222,6 +213,7 @@ class ObservationQueryResource(RestartRResource):
                     text/plain:
                         schema:
                             type: string
+
         """
         response = {}
         try:
@@ -229,33 +221,33 @@ class ObservationQueryResource(RestartRResource):
             authenticated = request.headers['X-API-Key'] == self.api_key
             assert authenticated, f"API Key presented does not match key configured for this system."
             response = {
-                "data": json.loads(JSONEncoder().encode(
-                    [x for x in observation.db.observation.find(obj)]
+                "data" : json.loads (JSONEncoder().encode(
+                    [ x for x in observation.db.observation.find (obj) ]
                 ))
             }
         except Exception as e:
-            response = self.create_response(
+            response = self.create_response (
                 exception=e,
                 message=f"Query failed: {json.dumps(request.json, indent=2)}.")
         return response
 
 
 """ Register endpoints. """
-resource_kw_args = {'api_key': APIKey.get_key()}
+resource_kw_args={ 'api_key' : APIKey.get_key () }
 api.add_resource(ObservationResource, '/observation', resource_class_kwargs=resource_kw_args)
 api.add_resource(ObservationQueryResource, '/query', resource_class_kwargs=resource_kw_args)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Genomic Observation API')
-    parser.add_argument('-p', '--port', type=int, help='Port to run service on.', default=5552)
-    parser.add_argument('-d', '--debug', help="Debug log level.", default=False, action='store_true')
-    args = parser.parse_args()
+   parser = argparse.ArgumentParser(description='Genomic Observation API')
+   parser.add_argument('-p', '--port',  type=int, help='Port to run service on.', default=5552)
+   parser.add_argument('-d', '--debug', help="Debug log level.", default=False, action='store_true')
+   args = parser.parse_args ()
 
-    debug = False
-    """ Configure """
-    if args.debug:
-        debug = True
-        logging.basicConfig(level=logging.DEBUG)
-        logger.debug(f"-- log level debug")
-    logger.info(f"starting RestartR on port={args.port} with debug={args.debug}")
-    app.run(host='0.0.0.0', port=args.port, debug=debug, threaded=True)
+   debug = False
+   """ Configure """
+   if args.debug:
+       debug = True
+       logging.basicConfig(level=logging.DEBUG)
+       logger.debug (f"-- log level debug")
+   logger.info (f"starting RestartR on port={args.port} with debug={args.debug}")
+   app.run(host='0.0.0.0', port=args.port, debug=debug, threaded=True)

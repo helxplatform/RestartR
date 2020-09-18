d5652402ccbec180dd29b186bb54d48d8febc6fd
Mani Singh <singh@rciadmins-MacBook-Pro.local>
2020-09-17 20:16:23 -0400

    updated mongo backup script

--- bin/mongo_backup.sh
+++ bin/mongo_backup.sh
@@ -4,11 +4,17 @@ backup_date=`date +'%m-%d-%Y'`
 
 cd /data/backup
 
-rm -rf backup-*
+if [ -d mongo_backups ]
+then
+  rm -rf mongo_backups
+fi
+
+mkdir mongo_backups
+cd mongo_backups
 
 mkdir backup-$backup_date
 
-mongodump --host restartr-api-service --port $1 --username $2 --password $3 --out /data/backup/backup-$backup_date/dump
+mongodump --host restartr-api-service --port 27017 --username $1 --password $2 --out /data/backup/mongo_backups/backup-$backup_date/dump
 
 
 
